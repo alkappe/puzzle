f23aec28cbe348db3ff379422089f63ca1bb4c0e
alkappe <alka.cappellazzo@gmail.com>
2019-04-17 15:30:44 +0200

    added move

--- index.js
+++ index.js
@@ -10,19 +10,31 @@ allPieces.forEach(cell => cell.addEventListener('click', function () {
   var cellId = cell.id
   var stateEmpty = state[0]
   var stateClicked = state[cellId]
+  //console.log(typeof state);
 
-  console.log(stateEmpty, stateClicked)
+  //console.log(stateEmpty, stateClicked)
   var diffA = Math.abs(stateEmpty[0] - stateClicked[0])
   var diffB = Math.abs(stateEmpty[1] - stateClicked[1])
 
   var distance = diffA + diffB
-  console.log(distance)
+  //console.log(distance)
 
   if (distance === 1 ) {
     swapCells(cell)
     state[cell.id] = state[0]
     state[0] = stateClicked
   }
+
+
+  /*var hammertime = new Hammer(cellSwapable);
+
+  hammertime.on('swipe', function(ev) {
+    swapCells(cell)
+    console.log(ev)
+  })
+  hammertime.get('swipe').set({ direction: Hammer.DIRECTION_ALL});*/
+
+
 }))
 
 function swapCells(cell) {
